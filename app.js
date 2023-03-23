fb6d24b3bdb0e1a5e565c0ff048d0042875e07bf
Khubayan <kubayan73@gmail.com>
2023-03-23 09:57:48 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+// import fs from 'fs';
+// import express from 'express';
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+
+// app.get('/', (req, res) => {
+//   res.status(200).json({ message: 'Hello from the server!!', app: 'Natours' });
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    result: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
