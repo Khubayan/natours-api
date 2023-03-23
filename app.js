c66f95f3b76d30a3a673ec5fe158d87cdbdf8ef8
Khubayan <kubayan73@gmail.com>
2023-03-23 10:56:19 +0800

    Responding to URL parameters

--- app.js
+++ app.js
@@ -25,6 +25,25 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1; //a trick to convert string into number
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
 // POST
 app.post('/api/v1/tours', (req, res) => {
   // console.log(req.body);
