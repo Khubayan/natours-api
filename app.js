3dd80f4646af819286ef509c77b489524727e729
Khubayan <kubayan73@gmail.com>
2023-03-23 10:38:32 +0800

    Handle POST request

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 const express = require('express');
 
 const app = express();
+// middleware
+app.use(express.json());
 
 // app.get('/', (req, res) => {
 //   res.status(200).json({ message: 'Hello from the server!!', app: 'Natours' });
@@ -21,6 +23,25 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+// POST
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'succes',
+        data: newTour,
+      });
+    }
+  );
 });
 
 const port = 3000;
