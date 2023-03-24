ee39c7cc2eaf7d7c498c1d85ea0c1324c2cad511
Khubayan <kubayan73@gmail.com>
2023-03-24 11:44:47 +0800

    Creating and Mounting Multiple Routes

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
-// MIDDLEWARES
 
+// MIDDLEWARES
 app.use(morgan('dev'));
 
 app.use(express.json());
@@ -134,26 +134,15 @@ const deleteUser = (req, res) => {
 };
 
 // ROUTES
-// app.get('/api/v1/tours', getAllTours);
-// app.post('/api/v1/tours', createTour);
-// app.get('/api/v1/tours/:id', getTour);
-// app.patch('/api/v1/tours/:id', updateTour);
-// app.delete('/api/v1/tours/:id', deleteTour);
+const tourRouter = express.Router();
+const userRouter = express.Router();
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
-app.route('/api/v1/tours').get(getAllTours).post(createTour);
-app
-  .route('/api/v1/tours/:id')
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
-
-app.route('/api/v1/users').get(getAllUsers).post(createUser);
-
-app
-  .route('/api/v1/users/:id')
-  .get(getUser)
-  .patch(updateUser)
-  .delete(deleteUser);
+tourRouter.route('/').get(getAllTours).post(createTour);
+tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+userRouter.route('/').get(getAllUsers).post(createUser);
+userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
 // START THE SERVER
 const port = 3000;
