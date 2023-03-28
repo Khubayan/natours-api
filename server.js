6c013ea2d9b1e413eef5210e9dd97329a8b39614
Khubayan <kubayan73@gmail.com>
2023-03-28 21:33:18 +0800

    Updating Nodejs from v14x to 18x & Connecting DB to the App

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successful!'));
 
 // START THE SERVER
 const port = process.env.PORT || 3000;
