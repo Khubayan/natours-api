ca732155ae9e2b5b7def62e410897227ef67b39b
Khubayan <kubayan73@gmail.com>
2023-03-30 12:17:51 +0800

    Another Way Creating Documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,46 +1,37 @@
 // const fs = require('fs');
 const Tour = require('../models/tourModel');
 
-exports.checkBodyMiddleware = (req, res, next) => {
-  if (!req.body.name && !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Missing name or price',
-    });
-  }
-  next();
-};
-
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
     requestAt: req.requestTime,
-    // result: tours.length,
-    // data: {
-    //   tours,
-    // },
   });
 };
 
 exports.getTour = (req, res) => {
-  // const id = req.params.id * 1; //a trick to convert string into number
-  // const tour = tours.find((el) => el.id === id);
-
   res.status(200).json({
     status: 'success',
-    // data: {
-    //   tour,
-    // },
   });
 };
 
-exports.createTour = (req, res) => {
-  // console.log(req.body);
-  res.status(201).json({
-    status: 'succes',
-    // data: newTour,
-  });
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'succes',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+    console.log(err);
+  }
 };
 
 exports.updateTour = (req, res) => {
