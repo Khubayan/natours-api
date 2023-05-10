780e5ac0207d87531bd74f465853ae2a18ad1fcd
Khubayan <kubayan73@gmail.com>
2023-05-10 21:49:30 +0800

    Add comments as documentation.

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-// This middleware below will be applied to all endpoints since we don't specify any endpoint. Btw, our routes also a middleware but for certain endpoint.
+// This middleware below will be applied to all endpoints since we haven't specified any endpoint. By the way, our routes also act as middleware, but only for certain endpoints.
 app.use((req, res, next) => {
   console.log('Hallo from the middleware');
   next(); // The next function is used to continue the request/response cycle or execute the next middleware in the stack. If you don't call it, the cycle will be stuck forever at this point, and the server will never be able to return a response for the client's request.
@@ -22,11 +22,12 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
+  console.log(`A Request recieved at ${req.requestTime}`);
   next();
 });
 
 // ROUTES
-app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/tours', tourRouter); // This is where we mount the route. Any request that refers to 'http://domain/api/v1/tours/' will be directed to the tourRouter. Depending on the method and request, the specific router will be called to process the request. In the route folder, you can check which HTTP methods the tourRoute supports.
 app.use('/api/v1/users', userRouter);
 
 module.exports = app;
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
 
-const router = express.Router();
+const router = express.Router(); // this is were we define a route for our API's endpoint.
 
 // router.param('id', tourController.checkIdMiddleware);
 
